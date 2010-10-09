456225ebeb39715d7092f21eea460f80e7470f18
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 15:02:36 -0700

    rework unit test using classes

--- sqlite3.scala
+++ sqlite3.scala
@@ -1,39 +1,91 @@
-var db = Array(0L)
-Sqlite3C.open(":memory:", db)
-var stmt = Array(0L)
-Sqlite3C.prepare_v2(db(0), "create table foo (i integer, f double, t text);", stmt)
-Sqlite3C.step(stmt(0))
-Sqlite3C.finalize(stmt(0))
-Sqlite3C.prepare_v2(db(0), "insert into foo (i, f, t) values (1, 2.0, 'foo');", stmt)
-Sqlite3C.step(stmt(0))
-Sqlite3C.finalize(stmt(0))
-Sqlite3C.prepare_v2(db(0), "insert into foo (i, f, t) values (3, NULL, 'bar');", stmt)
-Sqlite3C.step(stmt(0))
-Sqlite3C.finalize(stmt(0))
-Sqlite3C.prepare_v2(db(0), "select * from foo;", stmt)
-var done = false
-while (!done) {
-    val r = Sqlite3C.step(stmt(0))
-    if (r == Sqlite3C.SQLITE_DONE) {
-        done = true;
+class Sqlite3Db {
+    private var db = Array(0L)
+    def open(path: String): Unit = {
+        Sqlite3C.open(":memory:", db)
     }
-    else {
-        var i = 0;
-        while (i < Sqlite3C.column_count(stmt(0))) {
-            if (i != 0)
-                System.out.print(" ")
-            val s = Sqlite3C.column_type(stmt(0), i) match {
-                case Sqlite3C.SQLITE_INTEGER => Sqlite3C.column_int(stmt(0), i).toString
-                case Sqlite3C.SQLITE_FLOAT => Sqlite3C.column_double(stmt(0), i).toString
-                case Sqlite3C.SQLITE_TEXT => Sqlite3C.column_text(stmt(0), i)
-                case Sqlite3C.SQLITE_NULL => "NULL"
-                case _ => "???"
-            }
-            System.out.print(s)
-            i = i + 1
+    def close(): Unit = {
+        Sqlite3C.close(db(0))
+        db(0) = 0
+    }
+    override def finalize(): Unit = {
+        if (db(0) != 0)
+            close()
+    }
+    class Result(dbc: Sqlite3Db) {
+        var db: Sqlite3Db = dbc
+        var stmt = Array(0L)
+        var r = Sqlite3C.SQLITE_DONE
+        def done(): Boolean = {
+            r == Sqlite3C.SQLITE_DONE
         }
-        System.out.println()
+        def next(): Unit = {
+            r = Sqlite3C.step(stmt(0))
+            if (r != Sqlite3C.SQLITE_DONE && r != Sqlite3C.SQLITE_ROW)
+                throw new Exception("unexpected result: " + r)
+        }
+        def close(): Unit = {
+            Sqlite3C.finalize(stmt(0))
+            stmt(0) = 0
+        }
+        override def finalize(): Unit = {
+            if (stmt(0) != 0)
+                close()
+        }
+        def columnCount(): Int = {
+            Sqlite3C.column_count(stmt(0))
+        }
+        def columnType(i: Int): Int = {
+            Sqlite3C.column_type(stmt(0), i)
+        }
+        def asInt(i: Int): Int = {
+            Sqlite3C.column_int(stmt(0), i)
+        }
+        def asDouble(i: Int): Double = {
+            Sqlite3C.column_double(stmt(0), i)
+        }
+        def asText(i: Int): String = {
+            Sqlite3C.column_text(stmt(0), i)
+        }
+    }
+    def query(sql: String): Result = {
+        var res = new Result(this)
+        val rc = Sqlite3C.prepare_v2(db(0), sql, res.stmt)
+        res.next()
+        res
+    }
+    def execute(sql: String): Unit = {
+        var stmt = query(sql)
+        while (!stmt.done()) {
+            stmt.next()
+        }
+        stmt.close()
     }
 }
-Sqlite3C.finalize(stmt(0))
-Sqlite3C.close(db(0))
+
+var db = new Sqlite3Db
+db.open(":memory:")
+db.execute("create table foo (i integer, f double, t text);")
+db.execute("insert into foo (i, f, t) values (1, 2.0, 'foo');")
+db.execute("insert into foo (i, f, t) values (3, NULL, 'bar');")
+var res = db.query("select * from foo")
+while (!res.done()) {
+    var i = 0;
+    while (i < res.columnCount()) {
+        if (i != 0)
+            System.out.print(" ")
+        val s = res.columnType(i) match {
+            case Sqlite3C.SQLITE_INTEGER => res.asInt(i).toString
+            case Sqlite3C.SQLITE_FLOAT => res.asDouble(i).toString
+            case Sqlite3C.SQLITE_TEXT => res.asText(i)
+            case Sqlite3C.SQLITE_NULL => "NULL"
+            case _ => "???"
+        }
+        System.out.print(s)
+        i = i + 1
+    }
+    System.out.println()
+    res.next()
+}
+res.close()
+db.close()
+
