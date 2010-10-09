3fd9f0dd6f08f19fbee7b64442dadc4661cef7f7
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 11:25:03 -0700

    check return codes

--- Sqlite3.java
+++ Sqlite3.java
@@ -1,20 +1,27 @@
 class Sqlite3 {
+    public static final int SQLITE_OK = 0;
+    public static final int SQLITE_DONE = 101;
+
     native static public int sqlite3_open(String path, long[] db);
     native static public int sqlite3_enable_load_extension(long db, int onoff);
     native static public int sqlite3_prepare_v2(long db, String sql, long[] stmt);
     native static public int sqlite3_step(long stmt);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         long db[] = {0};
         int r = sqlite3_open(":memory:", db);
-        System.out.println("r=" + r + ", db=" + Long.toHexString(db[0]));
+        if (r != SQLITE_OK)
+            throw new Exception("open failed: " + r);
         r = sqlite3_enable_load_extension(db[0], 1);
-        System.out.println("r=" + r);
+        if (r != SQLITE_OK)
+            throw new Exception("enable_load_extension failed: " + r);
         long stmt[] = {0};
         r = sqlite3_prepare_v2(db[0], "create table foo (bar iteger);", stmt);
-        System.out.println("r=" + r + ", stmt=" + Long.toHexString(stmt[0]));
+        if (r != SQLITE_OK)
+            throw new Exception("prepare_v2 failed: " + r);
         r = sqlite3_step(stmt[0]);
-        System.out.println("r=" + r);
+        if (r != SQLITE_DONE)
+            throw new Exception("step failed: " + r);
     }
 
     static {
