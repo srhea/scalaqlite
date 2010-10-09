54898546dda3781a3c1ac688418544a5b8f85122
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 12:44:13 -0700

    get rid of redundant sqlite3_ prefix

--- Sqlite3.java
+++ Sqlite3.java
@@ -9,69 +9,69 @@ class Sqlite3 {
     public static final int SQLITE_BLOB = 4;
     public static final int SQLITE_NULL = 5;
 
-    native static public int sqlite3_open(String path, long[] db);
-    native static public int sqlite3_close(long db);
-    native static public int sqlite3_enable_load_extension(long db, int onoff);
-    native static public int sqlite3_prepare_v2(long db, String sql, long[] stmt);
-    native static public int sqlite3_step(long stmt);
-    native static public int sqlite3_finalize(long stmt);
-    native static public int sqlite3_column_count(long stmt);
-    native static public String sqlite3_column_name(long stmt, int n);
-    native static public int sqlite3_column_type(long stmt, int n);
-    native static public int sqlite3_column_int(long stmt, int n);
-    native static public float sqlite3_column_double(long stmt, int n);
-    native static public String sqlite3_column_text(long stmt, int n);
-    native static public String sqlite3_errmsg(long db);
+    native static public int open(String path, long[] db);
+    native static public int close(long db);
+    native static public int enable_load_extension(long db, int onoff);
+    native static public int prepare_v2(long db, String sql, long[] stmt);
+    native static public int step(long stmt);
+    native static public int finalize(long stmt);
+    native static public int column_count(long stmt);
+    native static public String column_name(long stmt, int n);
+    native static public int column_type(long stmt, int n);
+    native static public int column_int(long stmt, int n);
+    native static public float column_double(long stmt, int n);
+    native static public String column_text(long stmt, int n);
+    native static public String errmsg(long db);
 
     public static void main(String[] args) throws Exception {
         long db[] = {0};
-        int r = sqlite3_open(":memory:", db);
+        int r = open(":memory:", db);
         if (r != SQLITE_OK)
-            throw new Exception("open failed(" + r + "): " + sqlite3_errmsg(db[0]));
-        r = sqlite3_enable_load_extension(db[0], 1);
+            throw new Exception("open failed(" + r + "): " + errmsg(db[0]));
+        r = enable_load_extension(db[0], 1);
         if (r != SQLITE_OK)
-            throw new Exception("enable_load_extension failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("enable_load_extension failed(" + r + "): " + errmsg(db[0]));
         long stmt[] = {0};
-        r = sqlite3_prepare_v2(db[0], "create table foo (i integer, f double, t text);", stmt);
+        r = prepare_v2(db[0], "create table foo (i integer, f double, t text);", stmt);
         if (r != SQLITE_OK)
-            throw new Exception("prepare_v2 failed(" + r + "): " + sqlite3_errmsg(db[0]));
-        r = sqlite3_step(stmt[0]);
+            throw new Exception("prepare_v2 failed(" + r + "): " + errmsg(db[0]));
+        r = step(stmt[0]);
         if (r != SQLITE_DONE)
-            throw new Exception("step failed(" + r + "): " + sqlite3_errmsg(db[0]));
-        r = sqlite3_finalize(stmt[0]);
+            throw new Exception("step failed(" + r + "): " + errmsg(db[0]));
+        r = finalize(stmt[0]);
         if (r != SQLITE_OK)
-            throw new Exception("finalize failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("finalize failed(" + r + "): " + errmsg(db[0]));
 
-        r = sqlite3_prepare_v2(db[0], "insert into foo (i, f, t) values (1, 2.0, 'foo');", stmt);
+        r = prepare_v2(db[0], "insert into foo (i, f, t) values (1, 2.0, 'foo');", stmt);
         if (r != SQLITE_OK)
-            throw new Exception("prepare_v2 failed(" + r + "): " + sqlite3_errmsg(db[0]));
-        r = sqlite3_step(stmt[0]);
+            throw new Exception("prepare_v2 failed(" + r + "): " + errmsg(db[0]));
+        r = step(stmt[0]);
         if (r != SQLITE_DONE)
-            throw new Exception("step failed(" + r + "): " + sqlite3_errmsg(db[0]));
-        r = sqlite3_finalize(stmt[0]);
+            throw new Exception("step failed(" + r + "): " + errmsg(db[0]));
+        r = finalize(stmt[0]);
         if (r != SQLITE_OK)
-            throw new Exception("finalize failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("finalize failed(" + r + "): " + errmsg(db[0]));
 
-        r = sqlite3_prepare_v2(db[0], "select * from foo;", stmt);
+        r = prepare_v2(db[0], "select * from foo;", stmt);
         if (r != SQLITE_OK)
-            throw new Exception("prepare_v2 failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("prepare_v2 failed(" + r + "): " + errmsg(db[0]));
         for (;;) {
-            r = sqlite3_step(stmt[0]);
+            r = step(stmt[0]);
             if (r == SQLITE_DONE)
                 break;
             if (r != SQLITE_ROW)
-                throw new Exception("step failed(" + r + "): " + sqlite3_errmsg(db[0]));
-            for (int i = 0; i < sqlite3_column_count(stmt[0]); ++i) {
+                throw new Exception("step failed(" + r + "): " + errmsg(db[0]));
+            for (int i = 0; i < column_count(stmt[0]); ++i) {
                 if (i != 0) System.out.print(" ");
-                switch (sqlite3_column_type(stmt[0], i)) {
+                switch (column_type(stmt[0], i)) {
                    case SQLITE_INTEGER:
-                       System.out.print(Integer.toString(sqlite3_column_int(stmt[0], i)));
+                       System.out.print(Integer.toString(column_int(stmt[0], i)));
                        break;
                    case SQLITE_FLOAT:
-                       System.out.print(Double.toString(sqlite3_column_double(stmt[0], i)));
+                       System.out.print(Double.toString(column_double(stmt[0], i)));
                        break;
                    case SQLITE_TEXT:
-                       System.out.print(sqlite3_column_text(stmt[0], i));
+                       System.out.print(column_text(stmt[0], i));
                        break;
                    default:
                        throw new Exception("unknown column type");
@@ -79,13 +79,13 @@ class Sqlite3 {
             }
             System.out.println("");
         }
-        r = sqlite3_finalize(stmt[0]);
+        r = finalize(stmt[0]);
         if (r != SQLITE_OK)
-            throw new Exception("finalize failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("finalize failed(" + r + "): " + errmsg(db[0]));
 
-        r = sqlite3_close(db[0]);
+        r = close(db[0]);
         if (r != SQLITE_OK)
-            throw new Exception("close failed(" + r + "): " + sqlite3_errmsg(db[0]));
+            throw new Exception("close failed(" + r + "): " + errmsg(db[0]));
     }
 
     static {
