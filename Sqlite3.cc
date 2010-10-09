6467d185821a19deb634f2192928256b7ec11819
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 11:15:02 -0700

    wrap sqlite3_prepare_v2

--- Sqlite3.cc
+++ Sqlite3.cc
@@ -1,5 +1,6 @@
 #include "Sqlite3.h"
 #include <stdio.h>
+#include <string.h>
 #include <sqlite3.h>
 
 JNIEXPORT jint JNICALL
@@ -14,6 +15,7 @@ Java_Sqlite3_sqlite3_1open(JNIEnv *env, jclass cls, jstring jpath, jlongArray jd
         jlong a[] = {(jlong) db};
         env->SetLongArrayRegion(jdb, 0, 1, a);
     }
+    env->ReleaseStringUTFChars(jpath, cpath);
     return r;
 }
 
@@ -25,3 +27,21 @@ Java_Sqlite3_sqlite3_1enable_1load_1extension(JNIEnv *env, jclass cls, jlong jdb
     return sqlite3_enable_load_extension(db, onoff);
 }
 
+JNIEXPORT jint JNICALL
+Java_Sqlite3_sqlite3_1prepare_1v2(
+    JNIEnv *env, jclass cls, jlong jdb, jstring jsql, jlongArray jstmt)
+{
+    printf("sqlite3_prepare_v2\n");
+    sqlite3 *db = (sqlite3*) jdb;
+    jboolean iscopy;
+    const char *csql = env->GetStringUTFChars(jsql, &iscopy);
+    sqlite3_stmt *stmt;
+    // XXX: does strlen work with UTF8?
+    int r = sqlite3_prepare_v2(db, csql, strlen(csql), &stmt, NULL);
+    if (r == SQLITE_OK) {
+        jlong a[] = {(jlong) stmt};
+        env->SetLongArrayRegion(jstmt, 0, 1, a);
+    }
+    env->ReleaseStringUTFChars(jsql, csql);
+    return r;
+}
--- Sqlite3.java
+++ Sqlite3.java
@@ -1,6 +1,7 @@
 class Sqlite3 {
     native static public int sqlite3_open(String path, long[] db);
     native static public int sqlite3_enable_load_extension(long db, int onoff);
+    native static public int sqlite3_prepare_v2(long db, String sql, long[] stmt);
 
     public static void main(String[] args) {
         long db[] = {0};
@@ -8,6 +9,9 @@ class Sqlite3 {
         System.out.println("r=" + r + ", db=" + Long.toHexString(db[0]));
         r = sqlite3_enable_load_extension(db[0], 1);
         System.out.println("r=" + r);
+        long stmt[] = {0};
+        r = sqlite3_prepare_v2(db[0], "create table foo (bar iteger);", stmt);
+        System.out.println("r=" + r + ", stmt=" + Long.toHexString(stmt[0]));
     }
 
     static {
