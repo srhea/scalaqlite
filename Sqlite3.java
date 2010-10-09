806d1052b89d9781d489081e03a48241c5fca7f2
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 12:10:34 -0700

    do an insert

--- Sqlite3.java
+++ Sqlite3.java
@@ -19,7 +19,7 @@ class Sqlite3 {
         if (r != SQLITE_OK)
             throw new Exception("enable_load_extension failed(" + r + "): " + sqlite3_errmsg(db[0]));
         long stmt[] = {0};
-        r = sqlite3_prepare_v2(db[0], "create table foo (bar iteger);", stmt);
+        r = sqlite3_prepare_v2(db[0], "create table foo (i integer, f double, t text);", stmt);
         if (r != SQLITE_OK)
             throw new Exception("prepare_v2 failed(" + r + "): " + sqlite3_errmsg(db[0]));
         r = sqlite3_step(stmt[0]);
@@ -28,6 +28,17 @@ class Sqlite3 {
         r = sqlite3_finalize(stmt[0]);
         if (r != SQLITE_OK)
             throw new Exception("finalize failed(" + r + "): " + sqlite3_errmsg(db[0]));
+
+        r = sqlite3_prepare_v2(db[0], "insert into foo (i, f, t) values (1, 2.0, 'foo');", stmt);
+        if (r != SQLITE_OK)
+            throw new Exception("prepare_v2 failed(" + r + "): " + sqlite3_errmsg(db[0]));
+        r = sqlite3_step(stmt[0]);
+        if (r != SQLITE_DONE)
+            throw new Exception("step failed(" + r + "): " + sqlite3_errmsg(db[0]));
+        r = sqlite3_finalize(stmt[0]);
+        if (r != SQLITE_OK)
+            throw new Exception("finalize failed(" + r + "): " + sqlite3_errmsg(db[0]));
+
         r = sqlite3_close(db[0]);
         if (r != SQLITE_OK)
             throw new Exception("close failed(" + r + "): " + sqlite3_errmsg(db[0]));
