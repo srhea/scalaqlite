17d62a3fcb6ed72e0f06bcd015960e4c23dc77a3
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 15:46:26 -0700

    check for done on other Result calls

--- sqlite3.scala
+++ sqlite3.scala
@@ -14,16 +14,23 @@ class Sqlite {
     class Result(dbc: Sqlite, stmtc: Long) {
         private val db: Sqlite = dbc
         private var stmt: Long = stmtc
-        private var r = Sqlite3C.DONE
+        private var r = 0
         def done(): Boolean = {
-            r == Sqlite3C.DONE
+            stmt == 0 || r == Sqlite3C.DONE
+        }
+        private def assertNotDone(): Unit = {
+            if (done())
+                throw new Exception("already done")
         }
         def next(): Unit = {
+            assertNotDone()
             r = Sqlite3C.step(stmt)
             if (r != Sqlite3C.DONE && r != Sqlite3C.ROW)
                 throw new Exception("unexpected result: " + r)
         }
         def close(): Unit = {
+            if (stmt == 0)
+                throw new Exception("already closed")
             Sqlite3C.finalize(stmt)
             stmt = 0
         }
@@ -32,18 +39,23 @@ class Sqlite {
                 close()
         }
         def columnCount(): Int = {
+            assertNotDone()
             Sqlite3C.column_count(stmt)
         }
         def columnType(i: Int): Int = {
+            assertNotDone()
             Sqlite3C.column_type(stmt, i)
         }
         def asInt(i: Int): Int = {
+            assertNotDone()
             Sqlite3C.column_int(stmt, i)
         }
         def asDouble(i: Int): Double = {
+            assertNotDone()
             Sqlite3C.column_double(stmt, i)
         }
         def asText(i: Int): String = {
+            assertNotDone()
             Sqlite3C.column_text(stmt, i)
         }
     }
