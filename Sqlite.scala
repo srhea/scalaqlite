81fe9eb7efdb13a8b8d7622807a31fe5b3d6cd82
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 19:36:48 -0700

    cleaning up code

--- Sqlite.scala
+++ Sqlite.scala
@@ -1,36 +1,24 @@
 class Sqlite(path: String) {
     private var db = Array(0L)
-    assertSuccess("open", Sqlite3C.open(path, db));
-    private def assertSuccess(method: String, rc: Int): Unit = {
+    assertSuccess("open", Sqlite3C.open(path, db))
+    private def assertSuccess(method: String, rc: Int): Unit =
         if (rc != Sqlite3C.OK)
-            throw new Exception(method + " failed: " + errmsg())
-    }
-    private def assertOpen(): Unit = {
-        if (db(0) == 0)
-            throw new Exception("db is not open")
-    }
+            throw new Exception(method + " failed: " + Sqlite3C.errmsg(db(0)))
+    private def assertOpen: Unit = if (db(0) == 0) throw new Exception("db is not open")
     def close(): Unit = {
-        assertOpen()
-        val rc = Sqlite3C.close(db(0))
-        assertSuccess("close", rc);
+        assertOpen
+        assertSuccess("close", Sqlite3C.close(db(0)))
         db(0) = 0
     }
-    override def finalize(): Unit = {
-        if (db(0) != 0)
-            Sqlite3C.close(db(0)) // ignore result
-    }
+    override def finalize(): Unit = if (db(0) != 0) Sqlite3C.close(db(0))
+
     class Result(dbc: Sqlite, stmtc: Long) {
         private val db: Sqlite = dbc
         private var stmt: Long = stmtc
-        def done(): Boolean = {
-            stmt == 0
-        }
-        private def assertNotDone(): Unit = {
-            if (done())
-                throw new Exception("already done")
-        }
-        def next(): Unit = {
-            assertNotDone()
+        def done: Boolean = stmt == 0
+        private def assertNotDone: Unit = if (done) throw new Exception("already done")
+        def next: Unit = {
+            assertNotDone
             var r = Sqlite3C.step(stmt)
             if (r == Sqlite3C.DONE) {
                 Sqlite3C.finalize(stmt)
@@ -40,41 +28,38 @@ class Sqlite(path: String) {
                 throw new Exception("unexpected result: " + r)
             }
         }
-        override def finalize(): Unit = {
-            if (stmt != 0)
-                Sqlite3C.finalize(stmt)
-        }
+        override def finalize: Unit = if (stmt != 0) Sqlite3C.finalize(stmt)
         def columnCount(): Int = {
-            assertNotDone()
+            assertNotDone
             Sqlite3C.column_count(stmt)
         }
         def columnType(i: Int): Int = {
-            assertNotDone()
+            assertNotDone
             Sqlite3C.column_type(stmt, i)
         }
         def asInt(i: Int): Int = {
-            assertNotDone()
+            assertNotDone
             Sqlite3C.column_int(stmt, i)
         }
         def asDouble(i: Int): Double = {
-            assertNotDone()
+            assertNotDone
             Sqlite3C.column_double(stmt, i)
         }
         def asText(i: Int): String = {
-            assertNotDone()
+            assertNotDone
             Sqlite3C.column_text(stmt, i)
         }
         class Row(resc: Result) {
-            private val res = resc;
+            private val res = resc
             class Element(resc: Result, ic: Int) {
-                private val res = resc;
-                private val i = ic;
-                def typ(): Int = { res.columnType(i); }
-                def asInt(): Int = { res.asInt(i); }
-                def asDouble(): Double = { res.asDouble(i); }
-                def asText(): String = { res.asText(i); }
+                private val res = resc
+                private val i = ic
+                def typ: Int = res.columnType(i)
+                def asInt: Int = res.asInt(i)
+                def asDouble: Double = res.asDouble(i)
+                def asText: String = res.asText(i)
                 override def toString(): String = {
-                    typ() match {
+                    typ match {
                         case Sqlite3C.INTEGER => asInt.toString
                         case Sqlite3C.FLOAT => asDouble.toString
                         case Sqlite3C.TEXT => asText
@@ -83,43 +68,35 @@ class Sqlite(path: String) {
                     }
                 }
             }
-            def elt(i: Int): Element = { new Element(res, i); }
-            def foreach(f: Element => Unit): Unit = {
-                for (i <- 0 until res.columnCount()) {
-                    f(elt(i));
-                }
-            }
+            def elt(i: Int): Element = new Element(res, i)
+            def foreach(f: Element => Unit): Unit =
+                for (i <- 0 until res.columnCount())
+                    f(elt(i))
             def map[T](f: Element => T): List[T] = {
               var r: List[T] = Nil
-              for (i <- 0 until res.columnCount()) {
+              for (i <- 0 until res.columnCount())
                   r = f(elt(i)) :: r
-              }
               r.reverse
             }
         }
-        def foreach(f: Row => Unit): Unit = {
-            while (!done()) {
+        def foreach(f: Row => Unit): Unit =
+            while (!done) {
                 f(new Row(this))
-                next()
+                next
             }
-        }
     }
     def query(sql: String): Result = {
-        assertOpen()
+        assertOpen
         var stmt = Array(0L)
         val rc = Sqlite3C.prepare_v2(db(0), sql, stmt)
-        assertSuccess("prepare", rc);
+        assertSuccess("prepare", rc)
         var res = new Result(this, stmt(0))
-        res.next()
+        res.next
         res
     }
     def execute(sql: String): Unit = {
         var stmt = query(sql)
-        while (!stmt.done())
-            stmt.next()
-    }
-    def errmsg(): String = {
-        assertOpen()
-        Sqlite3C.errmsg(db(0))
+        while (!stmt.done)
+            stmt.next
     }
 }
