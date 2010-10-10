a37cb13d52aa6e23b82f4ce633e56e78d8485a94
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 18:15:20 -0700

    all caps for sql reserved words

--- test.scala
+++ test.scala
@@ -1,9 +1,9 @@
 var db = new Sqlite
 db.open(":memory:")
-db.execute("create table foo (i integer, f double, t text);")
-db.execute("insert into foo (i, f, t) values (1, 2.0, 'foo');")
-db.execute("insert into foo (i, f, t) values (3, NULL, 'bar');")
-for (row <- db.query("select * from foo;")) {
+db.execute("CREATE TABLE foo (i INTEGER, f DOUBLE, t TEXT);")
+db.execute("INSERT INTO foo (i, f, t) VALUES (1, 2.0, 'foo');")
+db.execute("INSERT INTO foo (i, f, t) VALUES (3, NULL, 'bar');")
+for (row <- db.query("SELECT * FROM foo;")) {
     var list = for (elt <- row) yield elt.toString
     println(list.reduceLeft(_ + " " + _))
 }
