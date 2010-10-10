910db29901ddff6606b0870c59e28d7f78e1e9e0
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 20:24:33 -0700

    new style using case classes

--- Sqlite.scala
+++ Sqlite.scala
@@ -1,3 +1,9 @@
+abstract class SqlValue
+case class SqlNull() extends SqlValue { override def toString: String = "NULL" }
+case class SqlInt(i: Int) extends SqlValue { override def toString: String = i.toString }
+case class SqlDouble(d: Double) extends SqlValue { override def toString: String = d.toString }
+case class SqlText(s: String) extends SqlValue { override def toString: String = s }
+
 class Sqlite(path: String) {
     private var db = Array(0L)
     assertSuccess("open", Sqlite3C.open(path, db))
@@ -16,6 +22,7 @@ class Sqlite(path: String) {
         private val db: Sqlite = dbc
         private var stmt: Long = stmtc
         def done: Boolean = stmt == 0
+        override def finalize: Unit = if (!done) Sqlite3C.finalize(stmt)
         private def assertNotDone: Unit = if (done) throw new Exception("already done")
         def next: Unit = {
             assertNotDone
@@ -28,59 +35,23 @@ class Sqlite(path: String) {
                 throw new Exception("unexpected result: " + r)
             }
         }
-        override def finalize: Unit = if (stmt != 0) Sqlite3C.finalize(stmt)
-        def columnCount(): Int = {
+        def row: IndexedSeq[SqlValue] = {
             assertNotDone
-            Sqlite3C.column_count(stmt)
-        }
-        def columnType(i: Int): Int = {
-            assertNotDone
-            Sqlite3C.column_type(stmt, i)
-        }
-        def asInt(i: Int): Int = {
-            assertNotDone
-            Sqlite3C.column_int(stmt, i)
-        }
-        def asDouble(i: Int): Double = {
-            assertNotDone
-            Sqlite3C.column_double(stmt, i)
-        }
-        def asText(i: Int): String = {
-            assertNotDone
-            Sqlite3C.column_text(stmt, i)
-        }
-        class Row(resc: Result) {
-            private val res = resc
-            class Element(resc: Result, ic: Int) {
-                private val res = resc
-                private val i = ic
-                def typ: Int = res.columnType(i)
-                def asInt: Int = res.asInt(i)
-                def asDouble: Double = res.asDouble(i)
-                def asText: String = res.asText(i)
-                override def toString(): String = {
-                    typ match {
-                        case Sqlite3C.INTEGER => asInt.toString
-                        case Sqlite3C.FLOAT => asDouble.toString
-                        case Sqlite3C.TEXT => asText
-                        case Sqlite3C.NULL => "NULL"
-                        case _ => "???"
-                    }
+            for (i <- 0 until Sqlite3C.column_count(stmt))
+                yield Sqlite3C.column_type(stmt, i) match {
+                    case Sqlite3C.INTEGER => SqlInt(Sqlite3C.column_int(stmt, i))
+                    case Sqlite3C.FLOAT => SqlDouble(Sqlite3C.column_double(stmt, i))
+                    case Sqlite3C.TEXT => SqlText(Sqlite3C.column_text(stmt, i))
+                    case Sqlite3C.NULL => SqlNull()
+                    case _ => throw new Exception("unsupported type")
                 }
-            }
-            def elt(i: Int): Element = new Element(res, i)
-            def foreach(f: Element => Unit): Unit =
-                for (i <- 0 until res.columnCount())
-                    f(elt(i))
-            def map[T](f: Element => T): IndexedSeq[T] =
-              for (i <- 0 until res.columnCount())
-                  yield f(elt(i))
         }
-        def foreach(f: Row => Unit): Unit =
+        def foreach(f: IndexedSeq[SqlValue] => Unit): Unit = {
             while (!done) {
-                f(new Row(this))
+                f(row)
                 next
             }
+        }
     }
     def query(sql: String): Result = {
         assertOpen
