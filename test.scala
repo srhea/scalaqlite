293d99d612b3be4e159c9f2763b5c2a657373f82
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-09 19:12:07 -0700

    clean up example a little more

--- test.scala
+++ test.scala
@@ -3,8 +3,6 @@ db.open(":memory:")
 db.execute("CREATE TABLE foo (i INTEGER, f DOUBLE, t TEXT);")
 db.execute("INSERT INTO foo (i, f, t) VALUES (1, 2.0, 'foo');")
 db.execute("INSERT INTO foo (i, f, t) VALUES (3, NULL, 'bar');")
-for (row <- db.query("SELECT * FROM foo;")) {
-    var list = for (elt <- row) yield elt.toString
-    println(list.reduceLeft(_ + " " + _))
-}
+for (row <- db.query("SELECT * FROM foo;"))
+    println(row.map(_.toString).reduceLeft(_ + " " + _))
 db.close()
