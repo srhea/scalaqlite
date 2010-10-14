9176e941ad49fd36be85fe528f6204d03216720b
Sean Rhea <sean.c.rhea@gmail.com>
2010-10-14 09:35:56 -0700

    add error msgs to asserts

--- Sqlite.scala
+++ Sqlite.scala
@@ -18,7 +18,7 @@ class SqliteResultSet(db: SqliteDb, stmtc: Long) {
     def done = stmt == 0
     override def finalize() { if (!done) Sqlite3C.finalize(stmt) }
     def next() {
-        assert(!done)
+        assert(!done, "already done")
         val r = Sqlite3C.step(stmt)
         if (r == Sqlite3C.DONE) {
             Sqlite3C.finalize(stmt)
@@ -29,7 +29,7 @@ class SqliteResultSet(db: SqliteDb, stmtc: Long) {
         }
     }
     def row: IndexedSeq[SqlValue] = {
-        assert(!done)
+        assert(!done, "already done")
         for (i <- 0 until Sqlite3C.column_count(stmt))
             yield Sqlite3C.column_type(stmt, i) match {
                 case Sqlite3C.INTEGER => SqlInt(Sqlite3C.column_int(stmt, i))
@@ -44,17 +44,17 @@ class SqliteResultSet(db: SqliteDb, stmtc: Long) {
 
 class SqliteDb(path: String) {
     private val db = Array(0L)
-    Sqlite3C.open(path, db) ensuring (_ == Sqlite3C.OK)
+    Sqlite3C.open(path, db) ensuring (_ == Sqlite3C.OK, Sqlite3C.errmsg(db(0)))
     def close() {
-        assert(db(0) != 0)
-        Sqlite3C.close(db(0)) ensuring (_ == Sqlite3C.OK)
+        assert(db(0) != 0, "already closed")
+        Sqlite3C.close(db(0)) ensuring (_ == Sqlite3C.OK, Sqlite3C.errmsg(db(0)))
         db(0) = 0
     }
     override def finalize() { if (db(0) != 0) Sqlite3C.close(db(0)) }
     def query(sql: String): SqliteResultSet = {
-        assert(db(0) != 0)
+        assert(db(0) != 0, "db is closed")
         val stmt = Array(0L)
-        Sqlite3C.prepare_v2(db(0), sql, stmt) ensuring (_ == Sqlite3C.OK)
+        Sqlite3C.prepare_v2(db(0), sql, stmt) ensuring (_ == Sqlite3C.OK, Sqlite3C.errmsg(db(0)))
         val res = new SqliteResultSet(this, stmt(0))
         res.next()
         res
