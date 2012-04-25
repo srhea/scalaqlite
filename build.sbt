a648b7e4cbc0199320edf05d7ff8de2a7a3fc2d1
Sean Rhea <sean.c.rhea@gmail.com>
2012-04-24 17:35:27 -0700

    switch from JNI to JNA
    This change eliminates the need to build a C library, which vastly
    simplifies compiling scalaqlite. It also removes the need to set any
    library paths when running the code. Unfortunately, it's 2-6X slower
    in the performance tests:
    
    With JNI:
    inserts took 2089 ms: 47869.79415988511 rows/s
    query took 426 ms: 234741.78403755868 rows/s
    
    With JNA:
    inserts took 4488 ms: 22281.63992869875 rows/s
    query took 2289 ms: 43687.1996505024 rows/s

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ version := "0.3-SNAPSHOT"
 scalaVersion := "2.9.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.2" % "test"
+
+libraryDependencies += "net.java.dev.jna" % "jna" % "3.4.0"
--- src/main/scala/SqliteDb.scala
+++ src/main/scala/SqliteDb.scala
@@ -1,13 +1,12 @@
-// Copyright (c) 2010 Sean C. Rhea <sean.c.rhea@gmail.com>
+// Copyright (c) 2010-2012 Sean C. Rhea <sean.c.rhea@gmail.com>
 // All rights reserved.
 //
 // See the file LICENSE included in this distribution for details.
 
-// This file is the "meat" of scalaqlite.  I'm sure I should be doing something
-// more sophisticated about packages and file names, but I haven't quite sorted
-// out how they work in Scala yet.  Hopefully they're nicer than in Java.
-
 package org.srhea.scalaqlite
+
+import com.sun.jna.Pointer
+import com.sun.jna.ptr.PointerByReference
 import scala.collection.mutable.ListBuffer
 
 class SqlException(msg: String) extends Exception(msg)
@@ -33,28 +32,28 @@ case class SqlDouble(d: Double) extends SqlValue {
 }
 case class SqlText(s: String) extends SqlValue { override def toString = s }
 
-class SqliteResultIterator(db: SqliteDb, private var stmt: Long)
+class SqliteResultIterator(db: SqliteDb, private var stmt: Pointer)
     extends Iterator[IndexedSeq[SqlValue]]
 {
     private var cachedRow: IndexedSeq[SqlValue] = null
 
-    assert(stmt != 0)
+    assert(stmt != null)
     advance()
 
     private def advance() {
-        cachedRow = Sqlite3C.step(stmt) match {
+        cachedRow = Sqlite3C.Instance.sqlite3_step(stmt) match {
             case Sqlite3C.ROW =>
-                (0 until Sqlite3C.column_count(stmt)).map { i =>
-                    Sqlite3C.column_type(stmt, i) match {
-                        case Sqlite3C.INTEGER => SqlInt(Sqlite3C.column_int(stmt, i))
-                        case Sqlite3C.FLOAT => SqlDouble(Sqlite3C.column_double(stmt, i))
-                        case Sqlite3C.TEXT => SqlText(Sqlite3C.column_text(stmt, i))
+                (0 until Sqlite3C.Instance.sqlite3_column_count(stmt)).map { i =>
+                    Sqlite3C.Instance.sqlite3_column_type(stmt, i) match {
+                        case Sqlite3C.INTEGER => SqlInt(Sqlite3C.Instance.sqlite3_column_int(stmt, i))
+                        case Sqlite3C.FLOAT => SqlDouble(Sqlite3C.Instance.sqlite3_column_double(stmt, i))
+                        case Sqlite3C.TEXT => SqlText(Sqlite3C.Instance.sqlite3_column_text(stmt, i))
                         case Sqlite3C.NULL => SqlNull()
                         case _ => sys.error("unsupported type")
                     }
                 }
             case Sqlite3C.DONE =>
-                Sqlite3C.finalize(stmt)
+                Sqlite3C.Instance.sqlite3_finalize(stmt)
                 null
             case Sqlite3C.ERROR =>
                 sys.error("sqlite error: " + db.errmsg)
@@ -74,28 +73,29 @@ class SqliteResultIterator(db: SqliteDb, private var stmt: Long)
 
     override def finalize() {
         if (hasNext)
-            Sqlite3C.finalize(stmt)
+            Sqlite3C.Instance.sqlite3_finalize(stmt)
     }
 }
 
 class SqliteDb(path: String) {
-    private val db = Array(0L)
-    Sqlite3C.open(path, db) ensuring (_ == Sqlite3C.OK, errmsg)
+    private val db = new PointerByReference
+    Sqlite3C.Instance.sqlite3_open(path, db) ensuring (_ == Sqlite3C.OK, errmsg)
     def close() {
-        assert(db(0) != 0, "already closed")
-        Sqlite3C.close(db(0)) ensuring (_ == Sqlite3C.OK, errmsg)
-        db(0) = 0
+        assert(db.getValue != null, "already closed")
+        Sqlite3C.Instance.sqlite3_close(db.getValue) ensuring (_ == Sqlite3C.OK, errmsg)
+        db.setValue(null)
     }
-    override def finalize() { if (db(0) != 0) Sqlite3C.close(db(0)) }
+    override def finalize() { if (db.getValue != null) Sqlite3C.Instance.sqlite3_close(db.getValue) }
     def query(sql: String): Iterator[IndexedSeq[SqlValue]] = {
-        assert(db(0) != 0, "db is closed")
-        val stmt = Array(0L)
-        Sqlite3C.prepare_v2(db(0), sql, stmt) ensuring (_ == Sqlite3C.OK, errmsg)
-        new SqliteResultIterator(this, stmt(0))
+        assert(db.getValue != null, "db is closed")
+        val stmt = new PointerByReference
+        val tail = new PointerByReference
+        Sqlite3C.Instance.sqlite3_prepare_v2(db.getValue, sql, sql.length, stmt, tail) ensuring (_ == Sqlite3C.OK, errmsg)
+        new SqliteResultIterator(this, stmt.getValue)
     }
     def execute(sql: String) { for (row <- query(sql)) () }
     def enableLoadExtension(on: Boolean) {
-        Sqlite3C.enable_load_extension(db(0), if (on) 1 else 0)
+        Sqlite3C.Instance.sqlite3_enable_load_extension(db.getValue, if (on) 1 else 0)
     }
-    def errmsg: String = if (db(0) == 0) "db not open" else Sqlite3C.errmsg(db(0))
+    def errmsg: String = if (db.getValue == null) "db not open" else Sqlite3C.Instance.sqlite3_errmsg(db.getValue)
 }
